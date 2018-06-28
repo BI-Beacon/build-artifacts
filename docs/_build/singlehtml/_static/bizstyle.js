f19f6af74efe63e35716fc357bd303936c297648
Travis CI <travis@travis-ci.org>
2018-06-28 15:48:16 +0000

    Automated build

--- docs/_build/singlehtml/_static/bizstyle.js
+++ docs/_build/singlehtml/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("BI-Beacon v0.26 documentation");
+        $("li.nav-item-0 a").text("BI-Beacon v0.50 documentation");
     }
 });
