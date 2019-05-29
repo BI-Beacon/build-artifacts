7458461bd6b59212ae34a137f95d10747f735e10
Travis CI <travis@travis-ci.org>
2019-05-29 08:17:11 +0000

    Automated build

--- docs/_build/singlehtml/_static/documentation_options.js
+++ docs/_build/singlehtml/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.56',
+    VERSION: 'v0.57',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
